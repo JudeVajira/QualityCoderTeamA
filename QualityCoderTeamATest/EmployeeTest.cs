6eb57602e5aa6cd54dbc3a7430a68a59a9674443
Lakshika.Sivapragasa <Lakshika>
2017-05-07 14:51:01 +0530

    Employee Test

---
+++ QualityCoderTeamATest/EmployeeTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace QualityCoderTeamATest
+{
+	[TestClass]
+	public class EmployeeTest
+	{
+		[TestMethod]
+		public void EmployeeImportDataFileRowCount()
+		{
+			
+		}
+	}
+}
