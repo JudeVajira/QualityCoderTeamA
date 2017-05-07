52731c9959d3d696b8c9d2a4c0a8d55beb556ed0
Lakshika.Sivapragasa <Lakshika>
2017-05-07 12:48:09 +0530

    Employee class added members

--- QualityCoderTeamA/Employee.cs
+++ QualityCoderTeamA/Employee.cs
@@ -7,25 +7,93 @@ namespace QualityCoderTeamA
 {
     public class Employee
     {
+		#region Members
+		private string		empID			= String.Empty;
+		private string		name			= String.Empty;
+		private string		gender			= String.Empty;
+		private DateTime	dob				= DateTime.MinValue.Date;
+		private string		designation		= String.Empty;
+		private double		basicSalary		= 0;
+		private double		epfContribution	= 0;
+		private string		phoneNumber		= String.Empty;
+		private string		address			= String.Empty;
+		#endregion
+
 		#region Properties
-		public String EmpID { get; set; }
-		public String Name { get; set; }
-		public String Gender { get; set; }
-		public DateTime DOB { get; set; }
-		public String Designation { get; set; }
-		public Double BasicSalary { get; set; }
-		public Double EPFContribution { get; set; }
-		public String PhoneNumber { get; set; }
-		public String Address { get; set; }
+		public String EmpID
+		{
+			get { return this.empID; }
+			set { this.empID = value; }
+		}
+		public String Name
+		{
+			get { return this.name; }
+			set { this.name = value; }
+		}
+		public String Gender
+		{
+			get { return this.gender; }
+			set { this.gender = value; }
+		}
+		public DateTime DOB
+		{			
+			get { return this.dob; }
+			set { this.dob = value; }
+		}
+		public String Designation
+		{
+			get { return this.designation; }
+			set { this.designation = value; }
+		}
+		public Double BasicSalary
+		{
+			get { return this.basicSalary; }
+			set { this.basicSalary = value; }
+		}
+		public Double EPFContribution
+		{
+			get { return this.epfContribution; }
+			set { this.epfContribution = value; }
+		}
+		public String PhoneNumber
+		{
+			get { return this.phoneNumber; }
+			set { this.phoneNumber = value; }
+		}
+		public String Address
+		{
+			get { return this.address; }
+			set { this.address = value; }
+		}
 		#endregion
 
 		#region Constructor
 
-		public Employee(string empId, string name, string gender, DateTime dob, string designation, double basicSalary, double epfContribution,
-						string phoneNember, string address)
+		public Employee()
 		{
 
 		}
+
+		public Employee(string empId, string name, string gender, DateTime dob, 
+						string designation, double basicSalary, double epfContribution, string phoneNember, 
+						string address)
+		{
+			this.empID				= empId;
+			this.name				= name;
+			this.gender				= gender;
+			this.dob				= dob;
+			this.designation		= designation;
+			this.basicSalary		= basicSalary;
+			this.epfContribution	= epfContribution;
+			this.phoneNumber		= phoneNember;
+			this.address			= address;
+
+		}
+
+		#endregion
+
+		#region Methods
+
 
 		#endregion
 
