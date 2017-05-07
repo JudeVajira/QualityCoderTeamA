f6ceda0c975be044c333031e30abb02bba3da527
jude8899 <jude8899@ymail.com>
2017-05-07 14:48:42 +0530

    OT Calculation

--- QualityCoderTeamA/OverTime.cs
+++ QualityCoderTeamA/OverTime.cs
@@ -7,5 +7,17 @@ namespace QualityCoderTeamA
 {
     public class OverTime : Benefit
     {
+        public double BasicHourlyRate { get; set; }
+        public double OtRate { get; set; }
+        public double NoOfHours { get; set; }
+
+        public double CalculateRegularOT(Employee employee)
+        {
+            double basicSalaryHourlyRate = employee.BasicSalary / (20 * 8);
+            double eligibleNoOfOtHours = this.NoOfHours;
+            if (this.NoOfHours > 4)
+                eligibleNoOfOtHours = 4;
+            return BasicHourlyRate * eligibleNoOfOtHours * 1.5;
+        }
     }
 }
