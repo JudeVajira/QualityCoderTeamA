55616adb6e6de50ded306846284f9c16cc3c57eb
jude8899 <jude8899@ymail.com>
2017-05-07 12:25:42 +0530

    test

---
+++ QualityCoderTeamATest/TravelAllowanceTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QualityCoderTeamA;
+
+namespace QualityCoderTeamATest
+{
+    [TestClass]
+    public class TravelAllowanceTest
+    {
+        [TestMethod]
+        public void Calculate_Travel_Allowance_For_Employee()
+        {
+             Employee            
+        }
+    }
+}
