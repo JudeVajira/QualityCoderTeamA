561c36a264ddb807733abb0b2269dbb58f7a262f
Lakshika.Sivapragasa <Lakshika>
2017-05-07 11:33:04 +0530

    Const added

--- QualityCoderTeamA/Employee.cs
+++ QualityCoderTeamA/Employee.cs
@@ -7,31 +7,28 @@ namespace QualityCoderTeamA
 {
     public class Employee
     {
+		#region Properties
 		public String EmpID { get; set; }
-
 		public String Name { get; set; }
-
 		public String Gender { get; set; }
-
 		public DateTime DOB { get; set; }
-
 		public String Designation { get; set; }
-
 		public Double BasicSalary { get; set; }
-
 		public Double EPFContribution { get; set; }
-
 		public String PhoneNumber { get; set; }
-
 		public String Address { get; set; }
-		public User Use { get; set; }
-		
-		
+		#endregion
 
-        public void calculateSalary()
-        {
-            throw new System.NotImplementedException();
-        }
-    }
+		#region Constructor
+
+		public Employee(string empId, string name, string gender, DateTime dob, string designation, double basicSalary, double epfContribution,
+						string phoneNember, string address)
+		{
+
+		}
+
+		#endregion
+
+	}
 }
 
