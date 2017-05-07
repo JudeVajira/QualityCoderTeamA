a844e42d364fde6c0bfd41afd2ffef861a126b39
Jude Vajira <vajiragunasekera@gmail.com>
2017-05-07 12:57:23 +0530

    made csv file little

--- QualityCoderTeamA/CsvDataFile.cs
+++ QualityCoderTeamA/CsvDataFile.cs
@@ -12,6 +12,8 @@ namespace QualityCoderTeamA
         public List<Employee> importEmployeeDataFileCSV(string filepath)
         {
             var parser = new CsvParser(File.OpenText(filepath));
+
+            List<Employee> empList = new List<Employee>();
             while (true)
             {
                 var row = parser.Read();
@@ -20,9 +22,36 @@ namespace QualityCoderTeamA
                     break;
                 }
 
+                if (row[4] != "M" && row[4] != "F")
+                {
+                    continue;
+                }
+
+
+                var empId = row[0];
+                var name = row[1];
+                var gender = row[2];
+                DateTime? dob = null;
+
+                if (!string.IsNullOrWhiteSpace(row[3]))
+                {
+                    dob = Convert.ToDateTime(row[3]);
+                }
+                var designation = row[4];
+                var basicSalary = Convert.ToDouble(row[5]);
+                var test = row[6].TrimEnd('%');
+                var epfContribution = Convert.ToDouble(test);
+                var phoneNember = row[7];
+                var address = row[8];
+
+
+                Employee emp = new Employee(empId, name, gender, dob, designation, basicSalary, epfContribution, phoneNember, address);
+
+                empList.Add(emp);
+
 
             }
-            return null;
+            return empList;
         }
 
         public List<Employee> getEmployeeList()
--- QualityCoderTeamATest/CsvDataFileShould.cs
+++ QualityCoderTeamATest/CsvDataFileShould.cs
@@ -18,6 +18,10 @@ namespace QualityCoderTeamATest
 
             CsvDataFile csvData = new CsvDataFile();
             List<Employee> empList = csvData.importEmployeeDataFileCSV("D:\\Downloads ALT\\flatfilebackup1.csv");
+
+            Assert.IsNotNull(empList);
+            Assert.IsInstanceOfType(empList[0], typeof(Employee));
+
         }
     }
 }
